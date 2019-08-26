38199141e3f006e94df43a3497baae3675ea1d88
Vladimir Panteleev <git@thecybershadow.net>
2019-08-26 19:33:23 +0000

    retest-failed.sh: Bash strict mode

--- retest-failed.sh
+++ retest-failed.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -eEuo pipefail
+
 sqlite3 data/trend.s3db 'DELETE FROM [Commits] WHERE [Error] IS NOT NULL'
 sqlite3 data/trend.s3db 'DELETE FROM [Results] WHERE [Error] IS NOT NULL'
